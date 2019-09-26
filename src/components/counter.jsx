a4fbf1c2396a32f4153c22ea81660712564174ac
shery <ashaheer606@gmail.com>
2019-09-26 16:04:25 +0500

    Handling events added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,24 +2,47 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: 0
+  };
+  //binding the first event handler to use this state
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement = product => {
+    console.log(product);
+    console.log("Increment Clicked", this);
+    this.setState({ count: this.state.count + 1 });
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return "There is no tags!";
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
+  };
 
+  render() {
     return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
 
-  render() {
-    return <div>{this.renderTags()}</div>;
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 export default Counter;
