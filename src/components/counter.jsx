13adb9cebac6f692a132e8edf9f56a5bb8867b3d
shery <ashaheer606@gmail.com>
2019-09-26 18:17:26 +0500

    counters parent component added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,9 +10,9 @@ class Counter extends Component {
     this.handleIncrement = this.handleIncrement.bind(this);
   }
 
-  handleIncrement = product => {
-    console.log(product);
-    console.log("Increment Clicked", this);
+  handleIncrement = () => {
+    //console.log(product);
+    //console.log("Increment Clicked", this);
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -29,6 +29,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
